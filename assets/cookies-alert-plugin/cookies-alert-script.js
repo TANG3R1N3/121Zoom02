db7612e7d9947d1ff4849bdf086621c0ca09e922
TANG3R1N3 <38878360+TANG3R1N3@users.noreply.github.com>
2020-01-27 13:11:48 +0100

    create github pages

--- assets/cookies-alert-plugin/cookies-alert-script.js
+++ assets/cookies-alert-plugin/cookies-alert-script.js
@@ -1,2 +1,3 @@
 
-jQuery(function(){var a=jQuery;a.cookiesDirective({explicitConsent:!1,position:"bottom",duration:9999,limit:0,message:a("input[name=cookieData]").attr("data-cookie-text"),fontFamily:"Arial",fontColor:"#424a4d",fontSize:"13px",backgroundColor:"#eaeff1",backgroundOpacity:"80"});a(".cookieText").find("a").css({color:"#424a4d","text-decoration":"underline"});a("input[name=cookieData]").remove()});
+jQuery(function(){var b=jQuery,a=b("input[name=cookieData]");b.cookiesDirective({explicitConsent:!1,position:"bottom",duration:9999,limit:0,message:a.attr("data-cookie-text"),fontFamily:"Arial",fontColor:a.attr("data-cookie-colorText"),fontSize:"13px",backgroundColor:a.attr("data-cookie-colorBg"),backgroundOpacity:"",linkColor:a.attr("data-cookie-colorLink"),colorButton:a.attr("data-cookie-colorButton")});b(".cookieText").find("a").css({"text-decoration":a.attr("data-cookie-underlineLink")?"underline":
+""});a.remove()});
